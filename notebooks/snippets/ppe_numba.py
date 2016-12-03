9f0356260fbf201c08bbb2d02b6cff42f0310cd9
Gil Forsyth <gilforsyth@gmail.com>
2016-12-03 14:23:03 -0500

    swap indices throughout so it is [j, i] style
    to better match what we preach throughout the rest of the course

--- notebooks/snippets/ppe_numba.py
+++ notebooks/snippets/ppe_numba.py
@@ -1,6 +1,6 @@
 @jit(nopython=True)
 def pressure_poisson(p, b, l2_target):
-    I, J = b.shape
+    J, I = b.shape
 
     iter_diff = l2_target + 1
 
@@ -9,19 +9,19 @@ def pressure_poisson(p, b, l2_target):
         pn = p.copy()
         for i in range(1, I - 1):
             for j in range(1, J - 1):
-                p[i, j] = (.25 * (pn[i, j + 1] +
-                                  pn[i, j - 1] +
-                                  pn[i + 1, j] +
-                                  pn[i - 1, j]) -
-                                  b[i, j])
-
-        for i in range(I):
-            p[i, 0] = p[i, 1]
-            p[i, -1] = 0
+                p[j, i] = (.25 * (pn[j, i + 1] +
+                                  pn[j, i - 1] +
+                                  pn[j + 1, i] +
+                                  pn[j - 1, i]) -
+                                  b[j, i])
 
         for j in range(J):
-            p[0, j] = p[1, j]
-            p[-1, j] = p[-2, j]
+            p[j, 0] = p[j, 1]
+            p[j, -1] = p[j, -2]
+
+        for i in range(I):
+            p[0, i] = p[1, i]
+            p[-1, i] = 0
 
         if n % 10 == 0:
             iter_diff = numpy.sqrt(numpy.sum((p - pn)**2)/numpy.sum(pn**2))
